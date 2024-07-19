854161efac9ad0d089529f91d5fa9fbf27021ebc
Hatman7-T <paolo.laferla@studio.unibo.it>
2024-07-19 21:53:19 +0200

    Delete src directory

--- src/logicPuzzleSolver/exercises/RunCase.java
+++
@@ -1,9 +0,0 @@
-package logicPuzzleSolver.exercises;
-
-public abstract class RunCase {
-    public abstract Object[] getArgs();
-
-    public abstract Object getOutput();
-
-    public abstract Class<?> getOutputType();
-}
